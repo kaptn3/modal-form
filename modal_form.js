6bf4749e6adcb90eb759db0e882f859eadde93b6
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-06 23:03:14 +0300

    function of few modals

--- modal_form.js
+++ modal_form.js
@@ -3,33 +3,38 @@ function closeModal(){
 }
 
 $(document).ready(function() {
-  $('a#sign_link').click( function(event){
-    event.preventDefault();
-    $("body").addClass('fixed');
-    $('.modal')
-      .css("display", "flex")
-      .hide()
-      .fadeIn(400);    
-  });
+  function modalForm(modal, link) {
+    console.log($('#sign_form'));
+    link.click( function(event){
+      event.preventDefault();
+      $("body").addClass('fixed');
+      modal
+        .css("display", "flex")
+        .hide()
+        .fadeIn(400);    
+    });
 
-  $('span.close > a').click( function(event) {
-    event.preventDefault();
-    closeModal();
-  });
-
-  $(document).mouseup(function (e) {
-    let container = $(".modal_dialog");
-    if (container.has(e.target).length === 0){
+    $('span.close > a').click( function(event) {
+      event.preventDefault();
       closeModal();
-    }
-  });
+    });
 
-  document.addEventListener('keyup', function(e) {
-    let key = e.key + e.location;
-    if (key == 'Escape0') {
-      closeModal();
-    }
-  });
+    $(document).mouseup(function (e) {
+      let container = $(".modal_dialog");
+      if (container.has(e.target).length === 0){
+        closeModal();
+      }
+    });
+
+    document.addEventListener('keyup', function(e) {
+      let key = e.key + e.location;
+      if (key == 'Escape0') {
+        closeModal();
+      }
+    });
+  }
+
+  modalForm($('#sign_form'), $('a#sign_link'));
 });
 
 
