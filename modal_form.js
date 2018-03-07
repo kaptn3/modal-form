72e909763cdb02dda04c19351f8fd9a1dd148ee4
vikapitoshka <kapitonenko.vick@ya.ru>
2018-03-07 12:11:07 +0300

    fix bugs with few forms

--- modal_form.js
+++ modal_form.js
@@ -1,40 +1,37 @@
-function closeModal(){
-  $(".modal").fadeOut(400);
-}
-
 $(document).ready(function() {
-  function modalForm(modal, link) {
-    console.log($('#sign_form'));
-    link.click( function(event){
-      event.preventDefault();
-      $("body").addClass('fixed');
-      modal
-        .css("display", "flex")
-        .hide()
-        .fadeIn(400);    
-    });
+  let open_link = $('.open_modal');
 
-    $('span.close > a').click( function(event) {
-      event.preventDefault();
-      closeModal();
-    });
-
-    $(document).mouseup(function (e) {
-      let container = $(".modal_dialog");
-      if (container.has(e.target).length === 0){
-        closeModal();
-      }
-    });
-
-    document.addEventListener('keyup', function(e) {
-      let key = e.key + e.location;
-      if (key == 'Escape0') {
-        closeModal();
-      }
-    });
+  function closeModal() {
+    $(".modal").fadeOut(400);
   }
 
-  modalForm($('#sign_form'), $('a#sign_link'));
+  open_link.click( function(event){
+    let modal = $(this).attr('href');
+    event.preventDefault();
+    $("body").addClass('fixed');
+    $(modal)
+      .css("display", "flex")
+      .hide()
+      .fadeIn(400);    
+  });
+
+  $('span.close > a').click( function(event) {
+    event.preventDefault();
+    closeModal();
+  });
+
+  $(document).mouseup(function (e) {
+    let container = $(".modal");
+    if (container.has(e.target).length === 0){
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    let key = e.key + e.location;
+    if (key == 'Escape0') {
+      closeModal();
+    }
+  });
+
 });
-
-
